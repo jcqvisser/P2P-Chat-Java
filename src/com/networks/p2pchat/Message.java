12bea9bcb9692e6f40416b78e7509e1f7d94f1d6
Jacques Visser <jcqvisser@gmail.com>
2015-05-07 22:34:07 +0200

    Expanded Message class
    Added all getters, setters and private variables.

--- src/com/networks/p2pchat/Message.java
+++ src/com/networks/p2pchat/Message.java
@@ -80,11 +80,46 @@ public class Message {
 		// must make sure that original source is intact
 	}
 	
-	public messageType getMessageType() {return _mType}
-	public int getTTL() {return _ttl}
-	public Peer getOrigin() {return _origin}
-	public Peer getSource() {return _source}
-	public Peer getDestination() {return _destination}
-	public String getChannelID() {return _channelID}
-	public String getText() {return _text}
+	// Getters
+	public messageType getMessageType() {return _mType;}
+	public int getTTL() {return _ttl;}
+	public Peer getOrigin() {return _origin;}
+	public Peer getSource() {return _source;}
+	public Peer getDestination() {return _destination;}
+	public String getChannelID() {return _channelID;}
+	public String getText() {return _text;}
+	public String getFilename() {return _filename;}
+	public byte[] getData() {return _data;}
+	public String[] getChannels() {return _channels;}
+	public boolean getForwardable() {return _forwardable;}
+	public Peer[] getPeers() {return _peers;}
+	
+	// Setters
+	public void setMessageType(messageType a) {_mType = a;}
+	public void setTTL(int a) {_ttl = a;}
+	public void setOrigin(Peer a) {_origin = a;}
+	public void setSource(Peer a) {_source = a;}
+	public void setDestination(Peer a) {_destination = a;}
+	public void setChannelID(String a) {_channelID = a;}
+	public void setText(String a) {_text = a;}
+	public void setFilename(String a) {_filename = a;}
+	public void setData(byte[] a) {_data = a;}
+	public void setChannels(String[] a) {_channels = a;}
+	public void setForwardable(boolean a) {_forwardable = a;}
+	public void setPeers(Peer[] a) {_peers = a;}
+	
+	private messageType _mType;
+	private int _ttl;
+	private Peer _origin;
+	private Peer _source;
+	private Peer _destination;
+	private String _channelID;
+	private String _text;
+	private String _filename;
+	private byte[] _data;
+	private String[] _channels;
+	private boolean _forwardable;
+	private Peer[] _peers;
+	
+	
 }
