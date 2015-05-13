734d033b91b9af695f6ff38316bc6fea1296369e
Jacques Visser <jcqvisser@gmail.com>
2015-05-13 17:59:22 +0200

    Expanded Conversation object

--- src/com/networks/p2pchat/Conversation.java
+++ src/com/networks/p2pchat/Conversation.java
@@ -6,8 +6,12 @@ package com.networks.p2pchat;
 /**
  * Embodies a conversation
  * Holds sender and receiver objects
- * takes a conversationholder object in the constructor (this also holds the conversation object)
- * senders are not threaded, receivers are
+ * Senders are not threaded, receivers are
+ * 
+ * Takes a conversationholder object in the constructor (this also holds the conversation object)
+ * 
+ * Takes a Socket object, from which input and output streams are created.
+ * The input and outmut streams are used in constructing the sender and receiver objects.
  * @author jcqvi_000
  *
  */
@@ -18,19 +22,32 @@ public class Conversation implements Runnable{
 	 * Takes a conversationholder object which holds this conversation
 	 * Created from within a conversationholder
 	 */
-	public Conversation(ConversationHolder convHolder) {
+	public Conversation(ConversationHolder convHolder, Socket socket) {
+		_convHolder = convHolder;
+		_socket = socket;
+		
+		// TODO more things need to be  created 
+	}
+	
+	@Override
+	public void run() {
+		// TODO implement
 		
 	}
 	
-	/* holder class for conversations, conv's need to interact with it.
-	 * 
-	 */
-	ConversationHolder _convHolder;
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
+	public String getRecipientIp() {
+	// TODO implement	
 	}
-
+	
+	/* holder class for conversations, conv's need to interact with it.*/
+	ConversationHolder _convHolder;
+	
+	/*Socket object, used to create sender and receiver, might need to be closed at some point*/
+	Socket _socket;
+	
+	/*Sender object, not threaded, holds outputstream object pointed at a socket, called to send messsages */
+	Sender _sender;
+	
+	/*Receiver object, threaded, holds inputstream object pointed at _socket, receives messages*/
+	
 }
