fe7e0cd28f184956787864b6d863248e69a50040
Jacques Visser <jcqvisser@gmail.com>
2015-05-13 18:13:13 +0200

    Expanded Conversation and made Receiver and Sender

---
+++ src/com/networks/p2pchat/Receiver.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.networks.p2pchat;
+
+/**
+ * @author jcqvi_000
+ *
+ */
+public class Receiver {
+
+	/**
+	 * 
+	 */
+	public Receiver() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/networks/p2pchat/Sender.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.networks.p2pchat;
+
+/**
+ * @author jcqvi_000
+ *
+ */
+public class Sender {
+
+	/**
+	 * 
+	 */
+	public Sender() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
