4b815a2174032647af81b8735b853b3b1fa08b41
Jacques Visser <jcqvisser@gmail.com>
2015-05-13 21:23:08 +0200

    Updated Peer
    Removed port and added Javadocs comments

--- src/com/networks/p2pchat/Peer.java
+++ src/com/networks/p2pchat/Peer.java
@@ -2,43 +2,48 @@ package com.networks.p2pchat;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * Peer class that represents a user on the network.
+ * Holds an ID (String) and IP (String) for the user. 
+ * @author jcqvi_000
+ * @author Anthony
+ *
+ */
 @XmlRootElement( name = "peer")
 public class Peer {
-	// Public members.
-	public Peer(String id, String ipAddr, int port) {
+	
+	/* Standard constructor, takes an ID (STRING) and IP (String) */
+	public Peer(String id, String ipAddr) {
 		_id = id;
 		_ipAddr = ipAddr;
-		_port = port;
 	}
-	
+
+	/* No arguments constructor, used for JAXB Marshalling*/
 	public Peer() {}
 	
+	/* Function to get the ID String, used for JAXB Marshalling*/
 	public String getId() {
 		return _id;
 	}
 	
+	/* Function to get IP String, used in JAXB Marshalling*/
 	public String getIp() {
 		return _ipAddr;
 	}
 	
-	public int getPort() {
-		return _port;
-	}
-	
+	/*Function to set the ID String, used for JAXB Unmarshalling*/
 	public void setId(String id) {
 		_id = id;
 	}
 	
+	/* Function for setting the IP String, used in JAXB Unmarshalling*/
 	public void setIp(String ip) {
 		_ipAddr = ip;
 	}
 	
-	public void setPort(int port) {
-		_port = port;
-	}
-	
-	// Private members.
+	/* ID for user, a string*/
 	private String _id;
+	
+	/* IP for user IP, a string*/
 	private String _ipAddr;
-	private int _port;
 }
